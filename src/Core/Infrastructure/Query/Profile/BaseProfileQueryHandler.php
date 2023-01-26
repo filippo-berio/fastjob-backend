56ea80e8d9b572df4327584e2d57737e334e8b35
akuzin <akuzin@tradedealer.ru>
2023-01-26 17:53:22 +0500

    get user use case

--- src/Core/Infrastructure/Query/Profile/BaseProfileQueryHandler.php
+++ src/Core/Infrastructure/Query/Profile/BaseProfileQueryHandler.php
@@ -2,8 +2,7 @@
 
 namespace App\Core\Infrastructure\Query\Profile;
 
-use App\Auth\Entity\User as AuthUser;
-use App\Auth\Query\User\FindById\FindUserById;
+use App\Auth\UseCase\User\GetUserByIdUseCase;
 use App\Core\Domain\Entity\User;
 use App\Core\Infrastructure\Entity\Profile;
 use App\CQRS\Bus\QueryBusInterface;
@@ -14,15 +13,13 @@ abstract class BaseProfileQueryHandler implements QueryHandlerInterface
 {
     public function __construct(
         protected EntityManagerInterface $entityManager,
-        protected QueryBusInterface $queryBus,
+        protected GetUserByIdUseCase $getUserByIdUseCase,
     ) {
     }
 
     protected function fillProfile(Profile $profile): Profile
     {
-        // todo usecase
-        /** @var AuthUser $user */
-        $user = $this->queryBus->query(new FindUserById($profile->getUserId()));
+        $user = $this->getUserByIdUseCase->get($profile->getUserId());
         $profile->setUser(new User($user->getId(), $user->getPhone()));
         return $profile;
     }
